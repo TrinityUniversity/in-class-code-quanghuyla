18a1499286351578ae447fff55cf8218a68b1c7d
Quang Huy <qla@trinity.edu>
2024-04-25 23:16:12 -0500

    video 50

--- server/app/controllers/WebSocketChat.scala
+++ server/app/controllers/WebSocketChat.scala
@@ -8,20 +8,24 @@ import akka.actor.Actor
 import play.api.libs.streams.ActorFlow
 import akka.actor.ActorSystem
 import akka.stream.Materializer
+import actors.ChatActor
+import akka.actor.Props
+import actors.ChatManager
 
 @Singleton
 class WebSocketChat @Inject() (cc:ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc){
+    val manager = system.actorOf(Props[ChatManager], "Manager")
+
     def index = Action { implicit request=>
         Ok(views.html.chatPage())
     }
-/*
-    def socket = WebSocket.accept[String, String]{ request=>
-        println("Getting socket")
+    
+    def socket = WebSocket.accept[String, String]{ request =>
         ActorFlow.actorRef { out=>
-          ??
+          ChatActor.props(out, manager)
         
         }
     }
 }
-*/
+
 
--- server/public/javascrip/chat.js
+++
@@ -1,5 +0,0 @@
-const inputField = document.getElementById("chat-input");
-const outputArea = document.getElementById("chat-area");
-
-const socketRoute = document.getElementById("ws-route").value;
-const socket = new WebSocket(routesRoute.replace("http","ws"));
